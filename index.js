6cc1b1774e077a5f2bf156142e403d283113833e
Mirabilio <github@mirabilio.net>
2020-04-02 09:56:07 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const selectAll = require(`unist-util-select`).selectAll;
+const select = require(`unist-util-select`).select;
+
+module.exports = (refs, pluginOptions) => {
+  const {markdownAST} = refs;
+  const {parents} = pluginOptions;
+  const parent = refs.getNode(refs.markdownNode.parent);
+
+  if (parents && !parents.includes(parent.internal.owner)) return;
+
+  if (parent.internal.content.toLowerCase().startsWith('\n') ||
+    parent.internal.content.toLowerCase().startsWith('\r') ||
+    parent.internal.content.toLowerCase().startsWith('<p>')) return;
+
+  const p = select('root > *:only-child', markdownAST);
+
+  if (p && (p.type === 'paragraph')) {
+    markdownAST.children = selectAll('root > paragraph > *',
+        markdownAST );
+  }
+
+  return markdownAST;
+};
